430b89ac1598dd57c526cab65e381e642ab0d84f
latyshok <olatysh@alfabank.ru>
2025-06-02 22:57:50 +0300

    without JSX

--- result_university/src/App.jsx
+++ result_university/src/App.jsx
@@ -1,45 +1,65 @@
-import { useState } from "react";
+import { useState, createElement } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-    // Imperative
-    const [count, setCount] = useState(0);
+	// Imperative
+	const [count, setCount] = useState(0);
 
-    // Imperative
-    let now = new Date();
+	// Imperative
+	let now = new Date();
 
-    return (
-        <>
-            {/* Declarative */}
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-            <p>{now.getFullYear()}</p>
-        </>
-    );
+	return createElement(
+		"div",
+		null,
+		createElement(
+			"div",
+			null,
+			createElement(
+				"a",
+				{ href: "https://vite.dev", target: "_blank" },
+				createElement("img", {
+					src: viteLogo,
+					className: "logo",
+					alt: "Vite logo",
+				})
+			),
+			createElement(
+				"a",
+				{ href: "https://react.dev", target: "_blank" },
+				createElement("img", {
+					src: reactLogo,
+					className: "logo react",
+					alt: "React logo",
+				})
+			)
+		),
+		createElement("h1", null, "Vite + React"),
+		createElement(
+			"div",
+			{ className: "card" },
+			createElement(
+				"button",
+				{ onClick: () => setCount((count) => count + 1) },
+				"count is ",
+				count
+			),
+			createElement(
+				"p",
+				null,
+				"Edit ",
+				createElement("code", null, "src/App.jsx"),
+				" and save to test HMR"
+			)
+		),
+		createElement(
+			"p",
+			{ className: "read-the-docs" },
+			"Click on the Vite and React logos to learn more"
+		),
+		createElement("p", null, now.getFullYear())
+	);
 }
 
 export default App;
