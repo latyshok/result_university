411e59e10b47c5402455bef2c5b625118367d6f7
latyshok <olatysh@alfabank.ru>
2025-06-18 21:12:59 +0300

    Компоненты. Задание 2.

--- result_university/src/App.jsx
+++ result_university/src/App.jsx
@@ -1,65 +1,66 @@
-import { useState, createElement } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import {useState} from "react";
+import steps from './data.json'
+import styles from './app.module.css'
 
 function App() {
-	// Imperative
-	const [count, setCount] = useState(0);
+	const [activeIndex, setActiveIndex] = useState(0);
 
-	// Imperative
-	let now = new Date();
+	const isCurrentStepLast = steps.length - 1 === activeIndex;
+	const isCurrentStepFirst = 0 === activeIndex;
 
-	return createElement(
-		"div",
-		null,
-		createElement(
-			"div",
-			null,
-			createElement(
-				"a",
-				{ href: "https://vite.dev", target: "_blank" },
-				createElement("img", {
-					src: viteLogo,
-					className: "logo",
-					alt: "Vite logo",
-				})
-			),
-			createElement(
-				"a",
-				{ href: "https://react.dev", target: "_blank" },
-				createElement("img", {
-					src: reactLogo,
-					className: "logo react",
-					alt: "React logo",
-				})
-			)
-		),
-		createElement("h1", null, "Vite + React"),
-		createElement(
-			"div",
-			{ className: "card" },
-			createElement(
-				"button",
-				{ onClick: () => setCount((count) => count + 1) },
-				"count is ",
-				count
-			),
-			createElement(
-				"p",
-				null,
-				"Edit ",
-				createElement("code", null, "src/App.jsx"),
-				" and save to test HMR"
-			)
-		),
-		createElement(
-			"p",
-			{ className: "read-the-docs" },
-			"Click on the Vite and React logos to learn more"
-		),
-		createElement("p", null, now.getFullYear())
-	);
+	const onNextStep = () => {
+		if (!isCurrentStepLast) {
+			setActiveIndex((value) => value + 1);
+		}
+	}
+
+	const onPrevStep = () => {
+		if (!isCurrentStepFirst) {
+			setActiveIndex((value) => value - 1);
+		}
+	}
+
+	const onSelectStep = (index) => {
+		setActiveIndex(index);
+	}
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div className={styles['steps-content']}>
+						{steps[activeIndex].content}
+					</div>
+					<ul className={styles['steps-list']}>
+						{steps.map((item, index) => (
+							<li key={item.id} className={
+								`${styles['steps-item']} ${index <= activeIndex ? styles.done : ''} ${index === activeIndex ? styles.active : ''}`
+							}>
+								<button onClick={() => onSelectStep(index)}
+										className={styles['steps-item-button']}>{index + 1}</button>
+								Шаг {index + 1}
+							</li>
+						))}
+					</ul>
+					<div className={styles['buttons-container']}>
+						<button disabled={isCurrentStepFirst} onClick={onPrevStep} className={styles.button}>
+							Назад
+						</button>
+						{isCurrentStepLast ? (
+							<button onClick={() => {onSelectStep(0)}} className={styles.button}>
+								Начать сначала
+							</button>
+						) : (
+							<button onClick={onNextStep} className={styles.button}>
+								Далее
+							</button>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default App;
